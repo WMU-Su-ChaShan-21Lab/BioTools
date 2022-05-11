b96fb3e0ae1ec8cb1d17773ef8de71ac4c8c196a
diklios <47837462+diklios5768@users.noreply.github.com>
2022-05-11 19:36:33 +0800

    完善了WES mean_merge.py

--- scripts/WESimputation/mean_merge.py
+++ scripts/WESimputation/mean_merge.py
@@ -12,3 +12,27 @@
 @Motto          :   All our science, measured against reality, is primitive and childlike - and yet it is the most precious thing we have.
 """
 __auth__ = 'diklios'
+
+import os
+from collections import defaultdict
+
+import click
+
+
+@click.command()
+@click.argument('input_dir_path', type=click.Path(exists=True))
+def main(input_dir_path):
+    file_names = [file_name for file_name in os.listdir(input_dir_path) if file_name.endswith('.mean.txt.gz')]
+    file_names.sort()
+    chr_file_names = defaultdict(list)
+    for file_name in file_names:
+        chr_file_names[file_name.split('.')[0].split('-')[0]].append(file_name)
+    for chr_name, file_names in chr_file_names.items():
+        with open(os.path.join(input_dir_path, chr_name + '.mean.txt.gz'), 'w') as w:
+            for file_name in file_names:
+                with open(os.path.join(input_dir_path, file_name)) as f:
+                    w.write(f.read())
+
+
+if __name__ == '__main__':
+    main()
